8cf8c096963682ef64b06d9403c2c25febd99ba5
mindnervestech <mindnervestech@gmail.com>
2018-02-07 14:32:24 +0530

    change

--- src/main/java/com/weavers/duqhan/util/MailSender.java
+++ src/main/java/com/weavers/duqhan/util/MailSender.java
@@ -27,6 +27,7 @@ public class MailSender {
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.port", "465");
